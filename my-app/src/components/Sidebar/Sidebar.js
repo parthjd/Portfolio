c07116a7a6c5fa1b667ff104d744d89df48241d7
parthjd <parthjd28@gmail.com>
2020-04-08 14:06:45 -0400

    created sidebar

--- my-app/src/components/Sidebar/Sidebar.js
+++ my-app/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 import "./Sidebar.scss";
 
 export default function Sidebar() {
@@ -10,30 +11,53 @@ export default function Sidebar() {
         alt="profile picture of Parth Desai"
       />
       <p className="sidebar-name">Parth Desai</p>
-      <a className="sidebar-menu">
-        <i class="fas fa-home" />
-        Home
-      </a>
-      <a className="sidebar-menu">
-        <i class="fas fa-user" />
-        About
-      </a>
-      <a className="sidebar-menu">
-        <i class="fas fa-address-card" />
-        Resume
-      </a>
-      <a className="sidebar-menu">
-        <i class="fas fa-briefcase" />
-        Portfolio
-      </a>
-      <a className="sidebar-menu">
-        <i class="fas fa-book" />
-        Blog
-      </a>
-      <a className="sidebar-menu">
-        <i class="fas fa-envelope" />
-        Contact
-      </a>
+      <Router>
+        <Link className="sidebar-menu" to="/">
+          <i class="fas fa-home" />
+          Home
+        </Link>
+        <Link className="sidebar-menu" to="/about">
+          <i class="fas fa-user" />
+          About
+        </Link>
+        <Link className="sidebar-menu" to="/resume">
+          <i class="fas fa-address-card" />
+          Resume
+        </Link>
+        <Link className="sidebar-menu" to="/portfolio">
+          <i class="fas fa-briefcase" />
+          Portfolio
+        </Link>
+        <Link className="sidebar-menu" to="Blog">
+          <i class="fas fa-book" />
+          Blog
+        </Link>
+        <Link className="sidebar-menu" to="contact">
+          <i class="fas fa-envelope" />
+          Contact
+        </Link>
+      </Router>
+      <section className="social-media">
+        <a
+          href="https://github.com/parthjd"
+          target="_blank"
+          className="social-media-link"
+        >
+          {" "}
+          <i class="fab fa-github" />{" "}
+        </a>
+        <a
+          href="https://www.linkedin.com/in/parth-desai-60663178/"
+          target="_blank"
+          className="social-media-link"
+        >
+          {" "}
+          <i class="fab fa-linkedin" />
+        </a>
+      </section>
+      <p className="copyright">
+        2020 © Parth Desai <br /> All rights reserved.
+      </p>
     </div>
   );
 }
