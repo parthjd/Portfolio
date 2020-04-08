378681aacccb81ab83b5285883da950a1a9f5989
parthjd <parthjd28@gmail.com>
2020-04-08 11:41:34 -0400

    inital commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <header>
+      <Sidebar />
+    </header>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <img src="../Images/00100sPORTRAIT_00100_BURST20190411175908371_COVER_2-min.jpg" />
+      <p className="sidebar-name">Parth Desai</p>
+      <p className="sidebar-home">
+        <i class="fas fa-home" />
+        Home
+      </p>
+      <p className="sidebar-about">
+        <i class="fas fa-user" />
+        About
+      </p>
+    </div>
+  );
+}
