a20ce0a92e4b91523e693e2eb56c6dd34abdbea9
parthjd <parthjd28@gmail.com>
2020-04-15 02:05:14 -0400

    updated styles

--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -6,16 +6,18 @@ import "./Home.scss";
 export default function Home() {
   return (
     <main className="main-page">
-      <div id="video-overlay" />
-      <h2 className="home-name">Parth Desai</h2>
-      <Typing className="tag" speed={50}>
-        <span>I am a </span>
-        <span className="quality">Passionate Developer.</span>
-        <Typing.Backspace count={21} delay={1500} />
-        <span className="quality">Freelancer.</span>
-        <Typing.Backspace count={10} delay={1500} />
-        <span className="quality">ull Stack Developer.</span>
-      </Typing>
+      <div id="profile-picture">
+        {/* <img className="pic" src={require("../images/profile-pic.jpg")} /> */}
+        <h2 className="home-name">Parth Desai</h2>
+        <Typing className="tag" speed={50}>
+          <span>I am a </span>
+          <span className="quality">Passionate Developer.</span>
+          <Typing.Backspace count={21} delay={1500} />
+          <span className="quality">Freelancer.</span>
+          <Typing.Backspace count={10} delay={1500} />
+          <span className="quality">ull Stack Developer.</span>
+        </Typing>
+      </div>
     </main>
   );
 }
