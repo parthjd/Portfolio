75feaecc6e45c67d4668f1b189fa9ea1b56db2e7
parthjd <parthjd28@gmail.com>
2020-04-16 15:21:17 -0400

    did npm run build

---
+++ client/build/precache-manifest.95305e747d9ea51e689f70cf059f557e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf23c92766ffb48b773afd4fceb1115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7600d2dbf3b604b93259",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c74f9a4daf2bb17ab8e1",
+    "url": "/static/css/main.09680010.chunk.css"
+  },
+  {
+    "revision": "7600d2dbf3b604b93259",
+    "url": "/static/js/2.baaca00c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.baaca00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c74f9a4daf2bb17ab8e1",
+    "url": "/static/js/main.28fdf08d.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "9beede5586293b1046f1433daa00dc65",
+    "url": "/static/media/profile-pic.9beede55.png"
+  },
+  {
+    "revision": "04c3bf6c31d40951ea659cec51a2c74b",
+    "url": "/static/media/signature.04c3bf6c.png"
+  }
+]);
