27def23e4d5077ec30782ee9187e3869190c5bb1
parthjd <parthjd28@gmail.com>
2020-04-16 15:09:25 -0400

    updated index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Express will serve up production assets
-app.use(express.static("client/build"));
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 app.use(express.static(path.resolve(__dirname, "public")));
 
 // Express serve up index.html file if it doesn't recognize route
