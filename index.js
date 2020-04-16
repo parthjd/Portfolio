ceea84b59889e6329e9e7c09a154971896f70407
parthjd <parthjd28@gmail.com>
2020-04-16 14:38:31 -0400

    updated index.js removed if production

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === "production") {
-  // Express will serve up production assets
-  app.use(express.static("client/build"));
-  app.use(express.static(path.resolve(__dirname, "client", "src")));
+// Express will serve up production assets
+app.use(express.static("client/build"));
+app.use(express.static(path.resolve(__dirname, "client", "src")));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// Express serve up index.html file if it doesn't recognize route
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.post("/api/form", (req, res) => {
   nodemailer.createTestAccount((err, account) => {
