7c64b25ef85c5ac2322420b80c98d4257304685d
parthjd <parthjd28@gmail.com>
2020-04-16 13:52:31 -0400

    updated index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
-  express.static(path_join(__dirname, "/client/build"));
+  app.use("/static", express.static(path.join(__dirname, "public")));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require("path");
