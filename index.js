ca865dc86fa23b7619f4f4134b60e4d46d83ff23
parthjd <parthjd28@gmail.com>
2020-04-19 15:15:02 -0400

    updated send grid API

--- index.js
+++ index.js
@@ -24,33 +24,33 @@ app.post("/api/form", (req, res) => {
     <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
     <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:50% !important; width:50%; height:auto !important;" src="http://cdn.mcauto-images-production.sendgrid.net/739d627c883976c8/f6b348ff-fbad-412d-931c-efa64fb750aa/478x523.png" alt="" width="300" data-responsive="true" data-proportionally-constrained="false">
     </td>
-    <div style="font-family: inherit; text-align: left"><strong>Hi ${req.body.name}, Thanks for reaching out!</strong></div>
+    <div style="font-family: inherit; text-align: left"><strong>Hi ${req.body.name}, thanks for reaching out!</strong></div>
     <div style="font-family: inherit; text-align: left"><br></div>
     <div style="font-family: inherit; text-align: left"><span style="font-family: verdana, geneva, sans-serif">I will get back to you within 1-2 business days. For urgent inquires, please call me.&nbsp;</span></div>
     <div style="font-family: inherit; text-align: center"><br></div>
     <div style="font-family: inherit; text-align: inherit"><span style="font-family: verdana, geneva, sans-serif">Cheers,&nbsp;</span></div>
     <div style="font-family: inherit; text-align: inherit"><span style="font-family: verdana, geneva, sans-serif">Parth Desai</span></div>
-    <div style="font-family: inherit; text-align: inherit"><span style="font-family: verdana, geneva, sans-serif">+1 647 - 532 - 0880</span></div>
+    <div style="font-family: inherit; text-align: inherit"><span style="font-family: verdana, geneva, sans-serif">+1 647-532-0880</span></div>
     `;
-
     const adminEmail = `
     <div style="font-family: inherit"><span style="color: #efeaea; font-size: 15px">Hello,</span></div>
     <div style="font-family: inherit"><br></div>
     <div style="font-family: inherit"><span style="color: #efeaea; font-size: 15px">Client Name: ${req.body.name}</span></div>
     <div style="font-family: inherit"><span style="color: #efeaea; font-size: 15px">Client Email: ${req.body.email}</span></div>
-    <div style="font-family: inherit"><br></div> 
+    <div style="font-family: inherit"><br></div>
     <div style="font-family: inherit"><span style="font-family: lucida sans unicode,lucida grande,sans-serif; font-size: 16px">Client Message: ${req.body.message}</span></div>
     `;
-
     let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: "smtp.sendgrid.net",
       port: 587,
       auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
+        user: "apikey",
+        pass: process.env.API_PASSWORD,
+      },
+      tls: {
+        ciphers: "SSLv3",
       },
     });
-
     let mailOptions = {
       from: process.env.EMAIL, // sender address
       to: `${req.body.email}`, // list of receivers
@@ -58,27 +58,24 @@ app.post("/api/form", (req, res) => {
       text: req.body.message, // plain text body
       html: htmlEmail, // html body
     };
-
     let mailOption = {
-      from: process.env.EMAIL, // sender address
+      from: process.env.EMAIL,
       to: process.env.EMAIL, // list of receivers
       subject: `Message from ${req.body.name}`, // Subject line
       text: req.body.message, // plain text body
       html: adminEmail,
     };
-
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
         return console.log(err);
       }
-      console.log("Message sent: %s", info.messageId);
+      console.log("Message sent to client: %s", info.messageId);
     });
-
     transporter.sendMail(mailOption, (err, info) => {
       if (err) {
         return console.log(err);
       }
-      console.log("Message sent: %s", info.messageId);
+      console.log("Message sent to admin: %s", info.messageId);
     });
   });
 });
